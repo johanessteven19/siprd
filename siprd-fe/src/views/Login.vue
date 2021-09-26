e7e4367959e69a7893f785464ff4838b24f30105
Jerome Zebua <5338823-jeromeemmanuel@users.noreply.gitlab.com>
2021-09-26 21:51:27 +0700

    feat(frontend): styled the login page

--- siprd-fe/src/views/Login.vue
+++ siprd-fe/src/views/Login.vue
@@ -1,48 +1,83 @@
 <template>
-  <v-container>
-    <h1>Login</h1>
-  <v-form 
-    @submit.prevent="submitForm" 
-    ref="form"
-    v-model="valid"
-    lazy-validation
+  <div
+  class="d-flex align-center "
+  v-bind:style="{ width:'100%', height:'100%'}"
   >
-        <v-text-field
-          v-model="username"
-          label="username"
-          :rules="usernameRules"
-          required
-        ></v-text-field>
+    <v-card
+      flat
+      v-bind:style="{ width:'50%', 'background-color':'#8D38E3'}"
+      class="d-flex justify-center align-center"
+      height="100%"
+    >
+        <v-img
+          :src="require('../assets/bunderan.svg')"
+          class="bunderan"
+          contain
+          height="100%"
+          width="50%"
+        />
+    </v-card>
+    <v-card
+      flat
+      v-bind:style="{ width:'50%'}"
+    >
+          <v-container
+        style=
+        "margin: auto;
+        width: 60%;
+        padding: 150px 0;">
+          <h2>Masuk Dengan Akun Anda</h2>
+          <v-form 
+            @submit.prevent="submitForm" 
+            ref="form"
+            v-model="valid"
+            lazy-validation
+          >
+            <v-text-field
+              v-model="username"
+              label="username"
+              :rules="usernameRules"
+              required
+            ></v-text-field>
 
-        <v-text-field
-          v-model="password"
-          label="password"
-          :type="'password'"
-          :rules="passRules"
-          required
-        ></v-text-field>
-        <div v-if="password === '' || username === ''">
-          <v-btn
-            class="mr-4"
-            :disabled="true"
-            type="submit"
-            color="success"
-          >
-          submit
-        </v-btn>
-        </div>
-        <div v-else>
-          <v-btn
-            class="mr-4"
-            :disabled="false"
-            type="submit"
-            color="success"
-          >
-          submit
-        </v-btn>
-        </div>
-      </v-form>
-  </v-container>
+              <v-text-field
+                v-model="password"
+                label="password"
+                :type="'password'"
+                :rules="passRules"
+                required
+              ></v-text-field>
+                
+                <div style="margin-top: 10px;" v-if="password === '' || username === ''">
+                  <v-btn
+                    class="mr-4"
+                    :disabled="true"
+                    type="submit"
+                    color="#8D38E3"
+                    width= '100%'
+                  >
+                  masuk
+                </v-btn>
+                </div>
+                <div v-else>
+                  <v-btn
+                    class="mr-4 white--text"
+                    :disabled="false"
+                    type="submit"
+                    color="#8D38E3"
+                    width= '100%'
+                    
+                  >
+                  masuk
+                </v-btn>
+                
+              </div>
+            </v-form>
+            <p style="margin-top: 20px;" class="register">Tidak memiliki akun? <span> <a href="/Register" >Daftar</a></span></p>
+        </v-container>
+    </v-card>
+  </div>
+
 </template>
 
 <script>
@@ -69,7 +104,7 @@
           v => !!v || 'Username is required',
         ],
         passRules: [
-          v => !!v || 'Password is required',
+          v => !!v || 'Kata sandi tidak sesuai',
         ],
       }
     },
@@ -79,7 +114,7 @@
           this.errors.push("Username required.");
         }
         if (!this.password) {
-          this.errors.push("Kata sandi required.");
+          this.errors.push("Kata sandi tidak sesuai");
         }
         console.log(this.errors)
         if (this.errors.length) {
@@ -108,3 +143,4 @@
     }
   }
 </script>
+
