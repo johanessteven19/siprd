c57ee3f4407043223ab76e20a114e9f8876eeb5a
Yolanda W Sirait <yolanda.wintari@ui.ac.id>
2021-09-27 16:20:33 +0700

    fix(frontend): fixing variables name

--- siprd-fe/src/views/Register.vue
+++ siprd-fe/src/views/Register.vue
@@ -36,9 +36,9 @@
                 </v-text-field>
             </validation-provider>
 
-            <validation-provider v-slot="{ errors }" name="field_of_study">
+            <validation-provider v-slot="{ errors }" name="fieldOfStudy">
                 <v-text-field
-                    v-model="field_of_study"
+                    v-model="fieldOfStudy"
                     :error-messages="errors"
                     label="Bidang Keahlian"
                     required>   
@@ -60,7 +60,7 @@
             <v-col md="5" class="ml-auto">
             <validation-provider v-slot="{ errors }" name="fullname" rules="required">
                 <v-text-field
-                    v-model="full_name"
+                    v-model="fullName"
                     :error-messages="errors"
                     label="Nama Lengkap*"
                     required>   
@@ -162,11 +162,11 @@
             errors: [],
             email: null,
             username: null,
-            full_name: null,
+            fullName: null,
             password: null,
             nip: null,
             university: null,
-            field_of_study: null,
+            fieldOfStudy: null,
             position: null,
             posSelect: [
                 'Asisten Ahli',
@@ -197,10 +197,10 @@
                     "username": this.username,
                     "email": this.email,
                     "password": this.password,
-                    "full_name": this.full_name,
+                    "full_name": this.fullName,
                     "university": this.university,
                     "nip": this.nip,
-                    "field_of_study": this.field_of_study,
+                    "field_of_study": this.fieldOfStudy,
                     "position": this.position,
                     "role": this.role
                 }
