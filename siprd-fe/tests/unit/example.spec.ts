bf149aa19b065d4879e57b6aa46017ad9aa4b1c7
seto adhi <setoaprasetyo@gmail.com>
2021-10-31 20:15:51 +0700

    test: dashboard.vue test

--- siprd-fe/tests/unit/example.spec.ts
+++ siprd-fe/tests/unit/example.spec.ts
@@ -1,10 +1,21 @@
 import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
+import Dashboard from "@/views/Dashboard.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
     const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg },
+    });
+    expect(wrapper.text()).toMatch(msg);
+  });
+});
+
+describe("Dashboard.vue", () => {
+  it("renders props.msg when passed", () => {
+    const msg = "new message";
+    const wrapper = shallowMount(Dashboard, {
       propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
