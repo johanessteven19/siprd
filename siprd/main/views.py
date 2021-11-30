b2678de1cf1d2abec952ab508686dc4d46e15b92
Avatar Azka <avatar.putra@ui.ac.id>
2021-11-30 13:58:38 +0700

    feat(backend): Rename get assigned karils endpoint for reviewers
    url changed for get assigned karils

--- siprd/main/views.py
+++ siprd/main/views.py
@@ -484,13 +484,13 @@ class ManageKarilReview(APIView):
         serializer = ReviewSerializer(reviews, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-class GetReviewedKarils(APIView):
+class GetAssignedKarils(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
         user_data = get_user_data(request)
         try:
-            karils = KaryaIlmiah.objects.all().filter(reviewers__username = user_data['username'], status = 'Done')
+            karils = KaryaIlmiah.objects.all().filter(reviewers__username = user_data['username'])
         except KaryaIlmiah.DoesNotExist:
             return Response({'message': 'This review does not exist!'}, status=status.HTTP_404_NOT_FOUND)
         serializer = KaryaIlmiahSerializer(karils, many=True)
