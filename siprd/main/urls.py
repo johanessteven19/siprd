72f45c6b31b0ecb72268081e0449d90d2c35c4b7
unknown <johanes.steven@ui.ac.id>
2021-12-01 17:55:52 +0700

    feat(excel): add download option for peer review form, category not yet implemented

--- siprd/main/urls.py
+++ siprd/main/urls.py
@@ -19,6 +19,7 @@ urlpatterns = [
     path("api/manage-users/", views.ManageUsers.as_view()),
     path("api/manage-karil/", views.ManageKaril.as_view()),
     path("api/download", views.DownloadKaril.as_view()),
+    path("api/download-review-form", views.DownloadReviewForm.as_view()),
     path('api/google/', include('rest_social_auth.urls_jwt_pair')),
     path(r'^auth/', include('rest_framework_social_oauth2.urls')),
     path("api/is-user-exists", views.IsUserExist.as_view()),
